b75f2b1efcd4601ea34e7abe8e4e99d1b7e5c668
wahyumarlia <wahyumarlia230@yahoo.com>
2017-03-24 22:16:09 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tugas1informationretrivel
+# dataretrieval
+Tokenstem
---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='';
+$database='dbstbi';
+$conn=mysql_connect($host,$user,$pass);
+mysql_select_db($database);
+?>
